6b8755c3a38faa4484a2969a244a49a28fed398e
Christy Ray <christyray@jhmi.edu>
2024-02-11 01:40:03 -0500

    Move package functions into separate files
    Changes
    - Separating the NULL-related functions and the small list preview function
    into separate files to prepare for adding more functions to the package

--- R/null-arguments.R
+++ R/null-arguments.R
@@ -1,18 +1,3 @@
-#' Smaller list preview
-#'
-#' @param x A list or list-type object (e.g., a data frame)
-#' @param level The maximum number of list levels to display, defaults to 4
-#'
-#' @return The output of the `str()` function with specific options
-#' @export
-#'
-#' @examples
-#' x <- list(a = 1:3, b = list(c = 5:9))
-#' small(x)
-
-small <- function(x, level = 4) {
-  utils::str(x, max.level = level, list.len = 3, vec.len = 3, give.attr = FALSE)
-}
 
 #' Defaults for NULL values
 #'
